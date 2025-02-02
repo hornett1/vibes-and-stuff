1939c8c34c097ee147afb4ed241fcc4e7ed8515c
Матвей <hornett@host-192-168-1-7.sohonet.ua>
2025-02-02 19:30:30 +0200

    -

--- static/scripts/main_app/cassettes.js
+++ static/scripts/main_app/cassettes.js
@@ -8,16 +8,14 @@ function scrollCassetteIntoView(cassette) {
     const scrollTop = window.scrollY;
     const documentHeight = document.documentElement.scrollHeight;
     const cassetteHeight = rect.height;
-    const buffer = 50; // Добавляем буферную зону
+    const buffer = 50;
     
-    // Проверяем, находится ли кассета в начале документа
     if (rect.top + scrollTop < viewportHeight / 2) {
         window.scrollTo({
             top: Math.max(0, scrollTop + rect.top - buffer),
             behavior: 'smooth'
         });
     }
-    // Проверяем, находится ли кассета в конце документа
     else if (rect.bottom + scrollTop > documentHeight - viewportHeight / 2) {
         window.scrollTo({
             top: Math.min(
@@ -27,7 +25,6 @@ function scrollCassetteIntoView(cassette) {
             behavior: 'smooth'
         });
     }
-    // Для кассет в середине
     else {
         window.scrollTo({
             top: scrollTop + rect.top - (viewportHeight - cassetteHeight) / 2,
@@ -40,11 +37,10 @@ togglers.forEach(toggler => {
     toggler.addEventListener('click', function() {
         let cassette = this.closest('.cassette');
         if (cassette) {
-            // Закрываем все другие открытые кассеты
             cassettes.forEach(c => {
                 if (c !== cassette && c.classList.contains('is-flipped')) {
                     c.classList.remove('is-flipped');
-                    setTimeout(() => { c.style.zIndex = '1'; }, 500); // После анимации
+                    setTimeout(() => { c.style.zIndex = '1'; }, 500); 
                 }
             });
 
@@ -61,7 +57,7 @@ document.querySelectorAll('.hider').forEach(hider => {
             cassette.classList.toggle('is-flipped');
             setTimeout(() => { 
                 cassette.style.zIndex = '1';
-            }, 500); // После анимации
+            }, 500); 
         }
     });
 });
